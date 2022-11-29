97ed1e229cd9b668a2111a221f56e3d3031839e1
Crhistian Arenas <cmarenas@utp.edu.co>
2022-11-29 08:41:19 -0500

    se crea proyecto base en spring

---
+++ src/test/java/com/misiontic/fruver/FruverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.fruver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
